c1a3dbb8886eddd58204f0c4719cddace116c70e
ViniciussMelo <vinicius.smelo54@gmail.com>
2025-03-17 21:16:37 -0300

    feat: started class 01, react fundamentals

---
+++ fundamentals/01-reactjs-fundamentals/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1607705703571-c5a8695f18f6?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+      />
+
+      <div className={styles.profile}>
+        <strong>Vinicius Melo</strong>
+        <span>Full Stack developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
