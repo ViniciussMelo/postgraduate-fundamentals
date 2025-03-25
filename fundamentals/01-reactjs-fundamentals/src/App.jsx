25dd658adb6eb8ee0821ac25f428c072dce4cc36
ViniciussMelo <vinicius.smelo54@gmail.com>
2025-03-24 23:16:32 -0300

    feat: added props and useState

--- fundamentals/01-reactjs-fundamentals/src/App.jsx
+++ fundamentals/01-reactjs-fundamentals/src/App.jsx
@@ -5,6 +5,37 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/viniciussmelo.png',
+      name: 'Vinicius Melo',
+      role: 'Fullstack developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcar' },        
+    ],
+    publishedAt: new Date('2025-03-22 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcar' },        
+    ],
+    publishedAt: new Date('2025-03-23 20:00:00')
+  }
+]
+
 function App() {
   return (
     <>
@@ -12,7 +43,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
--- fundamentals/01-reactjs-fundamentals/src/components/Post.jsx
+++ fundamentals/01-reactjs-fundamentals/src/components/Post.jsx
@@ -1,39 +1,74 @@
-import { Avatar } from './Avatar'
-import { Comment } from './Comment'
+import { useState } from 'react';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
 
-import styles from './Post.module.css'
+import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
-export function Post() {
+import styles from './Post.module.css';
+
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein?!'
+  ]);
+
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
+
+
+  function handleCreateNewComment () {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar
-            src="https://github.com/viniciussmelo.png" 
+            src={author.avatarUrl}
           />
           <div className={styles.authorInfo}>
-            <strong>Vinicius Melo</strong>
-            <span>Full Stack Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="19 de março as 08:13" dateTime="2025-03-19 08:13:30">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="#">jane.design/doctorcare</a></p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '} {/* adicionar espaço via react */}
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>{' '} 
-        </p>
+        {content.map((line) => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea 
+          name="comment"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
           placeholder="Deixe um comentário"
         />
         <footer>
@@ -42,9 +77,14 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return (
+            <Comment 
+              key={comment}
+              content={comment}
+            />
+          )
+        })}
       </div>
     </article>
   )
