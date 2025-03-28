077144c89216303f3803220c88b94b6902075e6f
ViniciussMelo <vinicius.smelo54@gmail.com>
2025-03-27 22:41:32 -0300

    feat: added project with typescript

---
+++ fundamentals/01-reactjs-fundamentals-ts/src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+
+import { Avatar } from './Avatar';
+
+import styles from './Comment.module.css';
+
+interface ICommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: ICommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {    
+    // acessar o valor mais recente sem precisar esperar o render do react
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/viniciussmelo.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vinicius Melo</strong>
+              <time title="19 de março às 08:13" dateTime="2025-03-19 08:13:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp/>
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
