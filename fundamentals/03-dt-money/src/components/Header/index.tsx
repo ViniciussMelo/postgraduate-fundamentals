4d22ace186f07ea8869061a3db6f62c6157d25e1
ViniciussMelo <vinicius.smelo54@gmail.com>
2025-05-31 14:23:18 -0300

    feat: finished layout

--- fundamentals/03-dt-money/src/components/Header/index.tsx
+++ fundamentals/03-dt-money/src/components/Header/index.tsx
@@ -1,7 +1,26 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+import { NewTransactionModal } from '../NewTransactionModal';
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+
+import logo from '../../assets/logo.svg';
+
 export function Header() {
   return (
-    <>
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} alt="logo" />
 
-    </>
+        <Dialog.Root>
+          {/* to not create a new button, but use the one I'm passing to it */}
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
   )
 }
--- fundamentals/03-dt-money/src/pages/Transactions/index.tsx
+++ fundamentals/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,45 @@
+import { SearchForm } from '../../components/SearchForm';
+import { Summary } from '../../components/Summary';
 import { Header } from '../../components/Header';
+
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
 
 export function Transactions() {
   return (
     <>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td>- R$ 1.200,00</td>
+              <td>Casa</td>
+              <td>13/04/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   )
 }
